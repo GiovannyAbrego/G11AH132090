cefdaa8e4f5dae5afb2d6f077ec44394a815f588
Giovanny Abrego <gm.abregoh@gmail.com>
2021-11-07 22:53:59 -0600

    cambios expo

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-
+import 'firebase/firestore';
+//import firebase from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -28,8 +28,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
-/*export default !firebaseConfig.apps.lenght
+export default !firebaseConfig.apps.lenght
     ? firebaseConfig.initializeApp(firebaseConfig)
-    : firebaseConfig.app();*/
+    : firebaseConfig.app();
